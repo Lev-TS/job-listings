827e328035c10e2612f2d043fbbbe4bb3a975898
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-07-06 14:23:25 +0300

    Updates

---
+++ precache-manifest.8959c8a54524c9883b9e3ec57e2fa747.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64201bf0d9cba6585b4cf3e92ca008fd",
+    "url": "/job-listings/index.html"
+  },
+  {
+    "revision": "ea9524fad81228bc2f6c",
+    "url": "/job-listings/static/css/main.291e7acf.chunk.css"
+  },
+  {
+    "revision": "57a92973ff2cfbb71130",
+    "url": "/job-listings/static/js/2.9d4c9c63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-listings/static/js/2.9d4c9c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9524fad81228bc2f6c",
+    "url": "/job-listings/static/js/main.065d934f.chunk.js"
+  },
+  {
+    "revision": "653ab284b5aa837d6df7",
+    "url": "/job-listings/static/js/runtime-main.3e943e45.js"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/job-listings/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/job-listings/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/job-listings/static/media/icon-remove.ae67c678.svg"
+  }
+]);
